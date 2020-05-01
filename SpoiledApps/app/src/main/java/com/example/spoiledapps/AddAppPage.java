758b4a41654e0fcbd2396a3b53d5b5c873eaa701
THeKhanMann <35606098+THeKhanMann@users.noreply.github.com>
2020-05-01 14:53:42 -0400

    ManualEntryOption AddAppPage
    1st Rendition, NO checks on whats entered, no questions asked, will create app entry based on user input

--- SpoiledApps/app/src/main/java/com/example/spoiledapps/AddAppPage.java
+++ SpoiledApps/app/src/main/java/com/example/spoiledapps/AddAppPage.java
@@ -4,18 +4,59 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddAppPage extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddAppPage extends AppCompatActivity
+{
+    private static final String tag = "Add_App_Activity";
+    private static final String KEY_AppName = "App_Title";
+    private static final String KEY_CompanyName = "Company_Name";
+
+    private EditText editTextAppName;
+    private EditText editTextCompanyName;
+
+    private FirebaseFirestore db;
+
+    private Button addAppButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_app_page);
+
+        db = FirebaseFirestore.getInstance();
+
+        editTextAppName = findViewById(R.id.enterAppNameField);
+        editTextCompanyName = findViewById(R.id.enterAppCompanyField);
+
+        addAppButton = findViewById(R.id.AddNewAppButton);
+
+        addAppButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                String AppName = editTextAppName.getText().toString().trim();
+                                                String CompanyName = editTextCompanyName.getText().toString().trim();
+                                                ;
+                                                Map<String, Object> addNewAppToListing = new HashMap<>();
+
+                                                addNewAppToListing.put(KEY_AppName, AppName);
+                                                addNewAppToListing.put(KEY_CompanyName, CompanyName);
+
+                                                db.collection("Apps").document().set(addNewAppToListing);
+
+                                            }
+                                        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
