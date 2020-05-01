0c0d3b3cc5bc98f9621d12976f2c656ef73a50ad
samirosmani <49757810+samirosmani@users.noreply.github.com>
2020-05-01 05:25:07 -0400

    Added a bunch of stuff! (Update as of 5/1/2020)
    I added logout, reset buttons. I added email verification. I check if user credentials are correct. There is a small bug if someone could fix it please. When you click on the reset password button, a message pop ups. However, if you leave the field where you enter your email blank, the program crashes because empty string.

--- SpoiledApps/app/src/main/java/com/example/spoiledapps/HomePageActivity.java
+++ SpoiledApps/app/src/main/java/com/example/spoiledapps/HomePageActivity.java
@@ -1,8 +1,12 @@
 package com.example.spoiledapps;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,6 +15,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class HomePageActivity extends AppCompatActivity {
+    private Button mLogoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,16 @@ public class HomePageActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        mLogoutButton = findViewById(R.id.logoutButton);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });//end Logout Button implementation.
 
         getSupportActionBar().hide();
     }
---
+++ SpoiledApps/app/src/main/java/com/example/spoiledapps/LoginActivity.java
@@ -0,0 +1,156 @@
+package com.example.spoiledapps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button mLogin, mRegister, mResetPassword;
+    private EditText mUsername, mPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mLogin = findViewById(R.id.LoginButton);
+        mRegister = findViewById(R.id.RegisterButton);
+        mUsername = findViewById(R.id.username);
+        mPassword = findViewById(R.id.password);
+        mResetPassword = findViewById(R.id.forgotPassword);
+        progressBar = findViewById(R.id.progressBar2);
+        auth = FirebaseAuth.getInstance();
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
+            }
+        });//end Register Button implementation.
+
+        mResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText userEmail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetMessage = new AlertDialog.Builder(v.getContext());
+                passwordResetMessage.setTitle("Resetting your Password! It's okay, everyone forgets at some point!");
+                passwordResetMessage.setMessage("Please enter the email used for your account to reset account.");
+                passwordResetMessage.setView(userEmail);
+
+
+                //When user chooses "Yes" to reset password
+                passwordResetMessage.setPositiveButton("Yes please!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String emailAddress = userEmail.getText().toString().trim();
+                        auth.sendPasswordResetEmail(emailAddress).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(LoginActivity.this, "The Reset Email Link has been sent! Check your inbox. ", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "The Reset Email Link could not send! " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                //When user chooses "No" to reset password
+                passwordResetMessage.setNegativeButton("Never mind!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                            //We do nothing!!
+                    }
+                });
+
+                passwordResetMessage.create().show();
+            }//end onClick
+        });//end Reset Password Button implementation.
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username)) {
+                    mUsername.setError("Username is required!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required!");
+                    return;
+                }
+                if (password.length() < 8) {
+                    mPassword.setError("Your password must be at least 8 characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //We begin to authenticate username and password credentials.
+              //  if(someUser.isEmailVerified()) {
+                    auth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser someUser = auth.getCurrentUser();
+                                if(someUser.isEmailVerified()) {
+                                    Toast.makeText(LoginActivity.this, "You're now logged in!", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(), HomePageActivity.class));
+                                }//end mini if statement
+                                else {
+                                    someUser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(LoginActivity.this, "You are currently NOT VERIFIED which you need to do in order to log in! Another email link has been sent to your email. Please check your inbox. ", Toast.LENGTH_SHORT).show();
+                                            progressBar.setVisibility(View.INVISIBLE);
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        private String TAG = "";
+
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.d(TAG, "You are currently NOT VERIFIED! However, Email verification could NOT sent! "+ e.getMessage());
+                                            progressBar.setVisibility(View.INVISIBLE);
+                                        }
+                                    });
+                                }//end mini else statement
+                            } else {
+                                Toast.makeText(LoginActivity.this, "There was a problem logging in! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                    });//end implementation of auth.signInWithEmailAndPassword!
+                //}//end if(someUser.isEmailVerified())
+
+            }//end onClick
+        });//end Login Button implementation
+
+        getSupportActionBar().hide();
+    }
+}//end class
+
