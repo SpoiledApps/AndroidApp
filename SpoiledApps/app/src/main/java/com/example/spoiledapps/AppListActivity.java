5d9136347bd971e4bf6e8bacbcf6e11bc764208a
Greg-Salvesen2 <greg@latenightdesigns.com>
2020-05-08 15:48:26 -0400

    Merge

--- SpoiledApps/app/src/main/java/com/example/spoiledapps/AppListActivity.java
+++ SpoiledApps/app/src/main/java/com/example/spoiledapps/AppListActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ScrollView;
 
 public class AppListActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,15 @@ public class AppListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
+
+        getSupportActionBar().hide();
+
+        scrollView = findViewById(R.id.appListScroll);
+
+        auth = FirebaseAuth.getInstance();
+
+        
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
