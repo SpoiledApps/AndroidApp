7d98ff2fd1e90d25fb465b9037e227323556dc03
THeKhanMann <35606098+THeKhanMann@users.noreply.github.com>
2020-05-10 13:29:27 -0400

    Vote Functionality Methods Attempt
    Wrote methods to be called by onclick for helpful and unhelpful selection for voting by user

--- SpoiledApps/app/src/main/java/com/example/spoiledapps/AppDetailActivity.java
+++ SpoiledApps/app/src/main/java/com/example/spoiledapps/AppDetailActivity.java
@@ -16,6 +16,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -28,10 +30,14 @@ public class AppDetailActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private LinearLayout scrollView;
     private ArrayList<Review> reviewsList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_list);
+
+
 
         final String documentID = getIntent().getStringExtra("documentID");
         System.out.println("Swapped Intent ID: " + documentID);
@@ -79,6 +85,100 @@ public class AppDetailActivity extends AppCompatActivity {
                         }
                     }
                 });
+
+    }
+
+    public String fetchAuthUserID()
+    {
+        FirebaseAuth fAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+        String authUserID = fAuth.getCurrentUser().getUid();
+        return authUserID;
+    }
+    public String fetchUserDocumentId(String authUserID)
+    {
+        final String[] returnValue = new String[1];
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference usersRef = db.collection("Users");
+        Task<QuerySnapshot> idQuery = usersRef.whereEqualTo("True User ID", authUserID).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful())
+                {
+                    for (QueryDocumentSnapshot document: task.getResult())
+                    {
+                        //Map targetDocument = document.getData();
+                        //usersDocID = targetDocument.toString();
+                        String usersDocID = document.getData().toString();
+                        returnValue[0] = usersDocID;
+                    }
+                }
+            }
+        });
+        return returnValue[0];
+    }
+    public double fetchVoterReputationScore(String userDocNumber)
+    {
+        final double[] returnValue = new double[1];
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference reputationScoreRef = db.collection("Users").document(userDocNumber);
+        reputationScoreRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        double reputationScore = (double)documentSnapshot.get("Reputation_Score");
+                        returnValue[0] = reputationScore;
+                    }
+                });
+                return returnValue[0];
+    }
+    //Feed this method whichever ID is easily accessible from the vote click (Authid or docID)
+    public void updateReviewAuthorRepuationScore(String docID, boolean favorable, double voterWeightValue)
+    {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final boolean positive = favorable;
+        final double weight = voterWeightValue;
+        final DocumentReference authorReputationRef = db.collection("Users").document(docID);
+        authorReputationRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        double currentReputationScore = (double)documentSnapshot.get("Reputation_Score");
+                        double newReputationScore;
+                        if(positive)
+                        {
+                            newReputationScore = (currentReputationScore+weight);
+                        }
+                        else
+                        {
+                            newReputationScore = (currentReputationScore-weight);
+                        }
+                        authorReputationRef.update("Reputation_Score", newReputationScore);
+
+                    }
+                });
+    }
+    public void VoteAction(boolean favorable, String authorDocID)
+    {
+        double voterWeightValue;
+        //Fetching Voter's Reputation Score
+        String userAuthID = fetchAuthUserID();
+        String userDocNum = fetchUserDocumentId(userAuthID);
+        double voterReputation = fetchVoterReputationScore(userDocNum);
+        if(voterReputation == 0.0)
+        {
+            voterWeightValue = 0.1;
+        }
+        else if(voterReputation < 0.0)
+        {
+            voterWeightValue = 0.01;
+        }
+        else
+        {
+            voterWeightValue = (voterReputation/10) + 0.1;
+        }
+
+        updateReviewAuthorRepuationScore(authorDocID,favorable, voterWeightValue);
 
     }
 }
