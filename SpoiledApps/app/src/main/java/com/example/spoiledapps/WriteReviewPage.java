bf2e2d5e19cc25d73352448cdd22229413c784bf
THeKhanMann <35606098+THeKhanMann@users.noreply.github.com>
2020-05-11 13:08:24 -0400

    Null Hotfix attempt 2
    Moved the codeblock elsewhere

--- SpoiledApps/app/src/main/java/com/example/spoiledapps/WriteReviewPage.java
+++ SpoiledApps/app/src/main/java/com/example/spoiledapps/WriteReviewPage.java
@@ -125,17 +125,7 @@ public class WriteReviewPage extends AppCompatActivity {
                     }
                 });
 
-                final double[] authorRepScore = new double[1];
-                DocumentReference authorRepScoreRef = db.collection("Users").document(userID);
-                authorRepScoreRef.get()
-                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                            @Override
-                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                double reputationScore = (double)documentSnapshot.get("Reputation_Score");
-                                authorRepScore[0] = reputationScore;
-                            }
-                        });
-                double useableRepScore = authorRepScore[0];
+
 
 
 
@@ -148,7 +138,19 @@ public class WriteReviewPage extends AppCompatActivity {
                 String leastFavFeat = editTextleastFavFeature.getText().toString().trim();
                 String freeform = editTextFreeform.getText().toString().trim();
 
-                //Insert Logic to Fetch AppID here
+                final double[] authorRepScore = new double[1];
+                DocumentReference authorRepScoreRef = db.collection("Users").document(userID);
+                authorRepScoreRef.get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>()
+                        {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot)
+                            {
+                                double reputationScore = (double)documentSnapshot.get("Reputation_Score");
+                                authorRepScore[0] = reputationScore;
+                            }
+                        });
+                double useableRepScore = authorRepScore[0];
 
 
                 Map<String, Object> reviewSubmission = new HashMap<>();
