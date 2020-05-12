6d8fecba97eee2ca41ebc34ed1c5df500c4aa321
samirosmani <49757810+samirosmani@users.noreply.github.com>
2020-05-12 03:12:35 -0400

    Cleaned the UI up a bit!

--- SpoiledApps/app/src/main/java/com/example/spoiledapps/WriteReviewPage.java
+++ SpoiledApps/app/src/main/java/com/example/spoiledapps/WriteReviewPage.java
@@ -95,14 +95,6 @@ public class WriteReviewPage extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         submitReviewButton.setOnClickListener(new View.OnClickListener() {
             @Override
